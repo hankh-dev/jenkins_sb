f4b7f40376f839ac496771e88c4bc2653f9aace8
hankh <hankh.dev@gmail.com>
2024-04-02 18:38:02 +0900

    first commit

---
+++ src/main/java/org/example/jenkins_sb/JenkinsSbApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkins_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSbApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/jenkins_sb/controller/TestController.java
@@ -0,0 +1,20 @@
+package org.example.jenkins_sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    List<String> all() {
+        List<String> strings = new ArrayList<>();
+        strings.add("hi");
+        strings.add("kh");
+        return strings;
+    }
+
+}
